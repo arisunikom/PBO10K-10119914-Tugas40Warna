8003d51a568a6f255721ac2f6e611eae98059a53
Aris DS <dropsuite@Dropsuites-MacBook-Pro.local>
2020-11-26 20:53:08 +0700

    initial commit

---
+++ src/main/java/tugas40/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas40;
+
+/**
+ *
+ * @author
+ * Aris Prabowo
+ * 10119914 - IF10K
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
